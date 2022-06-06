78b0112467c63c87d64ae260a45b46a1d5310d73
Jairo Garzon <jairito108@hotmail.com>
2022-06-06 00:28:58 -0500

    Finalizado reto de frases locas RESUBIDO

---
+++ fraseslocas.py
@@ -0,0 +1,15 @@
+#reto frases locas
+#1. definamos variables
+
+adj = input("Escribe un adjetivo: ")
+verbo1 = input("Escribe un verbo: ")
+verbo2 = input("Escribe otro verbo: ")
+sustantivoenplural = input("Escribe un sustantivo en plural: ")
+
+#2. frase en una variable
+
+frase = f"El futbol es tan {adj}, siempre me sube la emocion porque me gusta {verbo1} las jugadas. Aprende a {verbo2} en la PlayStation o consola y siente las {sustantivoenplural} de los jugadores"
+
+#3. imprimamos la frase
+
+print(frase)
